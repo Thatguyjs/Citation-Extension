faf25ae9e0c97a9ceab02a82ab8ac4356d68b7a7
Thatguyjs <46634731+Thatguyjs@users.noreply.github.com>
2020-03-28 14:56:17 -0600

    Add files via upload

--- format.js
+++ format.js
@@ -60,7 +60,7 @@ let CitationObject = {
 // Citation parsing & storage syntax
 
 /*
-	Format files:
+	Format Files:
 	 - All format files must begin with "FORMAT v.(version number)\r\n"
 	   where (version number) is the format version triplet (e.g. 00.00.00)
 	 - .fmt is the default format file extension
@@ -76,16 +76,16 @@ let CitationObject = {
 	 - To get a property of an element, add a colon (:) and property
 	   number. This works recursively; properties of properties are allowed
 	 - Escape any formatting characters with a backslash (\) prefixing
-	   them. This also applies to escaping backslashes (escaped = \\)
+	   them. This also applies to escaping backslashes (\\)
 
 
 	Element Numbers:
-	 title: 0
-	 url: 1
-	 authors: 2
-	 publishers: 3
-	 publishdate: 4
-	 accessdate: 5
+	 0: title
+	 1: url
+	 2: authors
+	 3: publishers
+	 4: publishdate
+	 5: accessdate
 
 
 	Element properties:
@@ -117,4 +117,68 @@ let CitationObject = {
 
 	Example Citation Format:
 	 :MLA8:2. "0" 3, 4, 1:0.;
+*/
+
+
+
+// Citation history parsing & storage syntax
+
+/*
+	History Files:
+	 - All history files must begin with "HISTORY v.(version number)\r\n"
+	   where (version number) is the format version triplet (e.g. 00.00.00)
+	 - .chf is the default file extension
+
+
+	Citation General Syntax:
+	 - All containers must be defined before citations begin (see Containers)
+	 - All citations start with their metadata (see Citation Metadata) and
+	   one colon (:) on each side
+	 - All citation must end with the group separator () followed by a
+	   semicolon (;)
+	 - Use the group separator (), a number (0-9), and a colon (:) to
+	   mark different citation properties
+	 - The order in which citation properties are listed does not matter
+	 - Escape any formatting characters with a backslash (\) prefixing
+	   them. This also applies to escaping backslashes (\\)
+
+
+	Containers:
+	 - Citations can belong to more than one container at a time
+	 - Containers start with a hashtag (#) followed by a colon (:)
+	 - Containers only store their name and end with a semicolon (;)
+
+
+	Element Numbers:
+	 0: title
+	 1: url
+	 2: authors
+	 3: publishers
+	 4: publish date
+	 5: access date
+
+
+	Citation Metadata:
+	 - All metadata elements are separated by the group separator ()
+	 - All metadata properties must be in the correct order
+	 - Metadata properties:
+	    - Citation format
+	    - Citation name
+	    - Date created (see Datatypes)
+	    - Date modified (see Datatypes)
+	    - Parent containers (Array, see Datatypes)
+
+
+	Datatypes:
+	 Dates:
+	    - DDMMYYYY where DD and MM are two characters and YYYY is four characters
+		- Dates are surrounded by angle brackets (< and >)
+	 Arrays - Surrounded by brackets, use comma separated values
+
+
+	Example History File (with added newlines):
+	 HISTORY v.00.00.00
+	 #:My container name;
+	 #:Second container;
+	 :MLA8My citation name<18032020><18032020>[My container name,Second container]:0:Some News Article Title1:https://article-url.com2:[Prefix,First,Middle,Last][Prefix,First,Middle,Last]3:[Publisher 1,Publisher 2]4:<11022016>5:<18032020>;
 */
