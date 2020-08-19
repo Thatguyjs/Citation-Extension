5ccbcdb4b09129b76a69e71273786a70ed1bb9cd
Thatguyjs <that.program@gmail.com>
2020-08-19 15:52:59 -0600

    Basic theme parsing finished
    The theme parser can now be used with .ctm files (string header + JSON body)

--- general/theme.js
+++ general/theme.js
@@ -28,6 +28,15 @@ const Theme = {
 	_loaded: {},
 
 
+	// Parse theme data
+	_parseTheme: function(data) {
+		return {
+			error: 0,
+			data: JSON.parse(data)
+		};
+	},
+
+
 	// Load a theme file
 	loadFile: function(name='default') {
 		fetch(`/storage/theme/${name}.ctm`).then(async (response) => {
@@ -50,17 +59,37 @@ const Theme = {
 				return;
 			}
 
-			// TODO: Parse theme data
+			let parsed = this._parseTheme(data.slice(this._headerLength));
+			if(parsed.error) return console.error(parsed.error);
+
+			this.load(parsed.data);
+
 		}).catch((error) => {
 			console.warn(`Could not load theme file: ${name}.ctm`);
+			console.error(error);
 			if(name !== 'default') Theme.load('default');
 		});
 	},
 
 
-	// Load theme data
-	load: function(data) {
+	// Load a theme
+	load: function(object, part="-") {
+		for(let o in object) {
+			let name = part + '-' + o;
 
+			if(!Array.isArray(object[o])) {
+				this.load(object[o], name);
+			}
+			else {
+				let colorString = "none";
+
+				if(object[o][0] === 'rgb') {
+					colorString = `rgb(${object[o][1]}, ${object[o][2]}, ${object[o][3]})`;
+				}
+
+				document.documentElement.style.setProperty(name, colorString);
+			}
+		}
 	}
 
 };
